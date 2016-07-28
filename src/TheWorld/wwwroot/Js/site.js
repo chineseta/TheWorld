d3d647f180a70308cbdbdc13d9cb7b1592ba0038
jack li <chineseta88@gmail.com>
2016-07-28 00:30:52 -0400

    Add jquery and underscore-min.js

--- src/TheWorld/wwwroot/Js/site.js
+++ src/TheWorld/wwwroot/Js/site.js
@@ -1,3 +1,34 @@
 ﻿//site js
-var ele = document.getElementById("username");
-ele.innerHTML = "jack li";
+(function () {
+    //var ele = $("#username");
+    //ele.text("jack");
+
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color : #888";
+
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
+
+    //var menItems = $("ul.menu li a");
+    //menItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+
+
+    });
+
+})();
