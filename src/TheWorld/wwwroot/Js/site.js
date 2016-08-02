49a7c2663add019644dee7d24fe92efaf95adf59
jack li <chineseta88@gmail.com>
2016-08-02 07:03:03 -0400

    Add mvc

--- src/TheWorld/wwwroot/Js/site.js
+++ src/TheWorld/wwwroot/Js/site.js
@@ -1,23 +1,23 @@
 ﻿//site js
 (function () {
-    //var ele = $("#username");
-    //ele.text("jack");
+    var ele = $("#username");
+    ele.text("jack");
 
-    //var main = $("#main");
-    //main.on("mouseenter", function () {
-    //    main.style = "background-color : #888";
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.style = "background-color : #7d3636";
 
-    //});
+    });
 
-    //main.on("mouseleave", function () {
-    //    main.style = "";
-    //});
+    main.on("mouseleave", function () {
+        main.style = "";
+    });
 
-    //var menItems = $("ul.menu li a");
-    //menItems.on("click", function () {
-    //    var me = $(this);
-    //    alert(me.text());
-    //});
+    var menItems = $("ul.menu li a");
+    menItems.on("click", function () {
+        var me = $(this);
+        alert(me.text());
+    });
     var $sidebarAndWrapper = $("#sidebar,#wrapper");
 
     $("#sidebarToggle").on("click", function () {
